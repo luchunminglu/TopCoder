f595090e0f620a47d98459870f692bca6d6ffe53
luchunminglu <luchunminglu@gmail.com>
2013-06-16 17:42:03 +0800

    c

--- TopCoder/TopCoder/Program.cs
+++ TopCoder/TopCoder/Program.cs
@@ -8,8 +8,9 @@ namespace TopCoder
     {
         static void Main(string[] args)
         {
-            ChickenOracle o = new ChickenOracle();
-            Console.WriteLine(o.theTruth(1000,500,250,250));
+            TicketPrinters printers = new TicketPrinters();
+            int result = printers.minTime(2, new int[] { 13, 26, 39, 13 }, new int[] { 123, 12, 32, 67, 1015 }, new int[] { 1, 2, 3, 4, 5 });
+            Console.WriteLine(result);
         }
     }
 }
---
+++ TopCoder/TopCoder/TicketPrinters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+public class TicketPrinters
+{
+    public int minTime(int currentPrinter, int[] printerDistance, int[] startingValues, int[] wantedValues)
+    {
+        List<int> wantedUnUsed = new List<int>();
+        for (int i = 0; i < wantedValues.Length; i++)
+        {
+            wantedUnUsed.Add(i);
+        }
+        return Time(currentPrinter, 0, 0, currentPrinter, currentPrinter, wantedUnUsed, printerDistance, startingValues, wantedValues);
+    }
+    private int Time(int currentPrinter, int passedTime, int lastTime, int lowPrinter, int highPrinter, List<int> wantedUnUsed, int[] printerDistance, int[] startingValues, int[] wantedValues)
+    {
+        if (wantedUnUsed.Count == 1)
+            return Math.Max(lastTime, Math.Abs(startingValues[currentPrinter] - wantedValues[wantedUnUsed[0]]) + 1 + passedTime);
+        int low = lowPrinter - 1;
+        int high = highPrinter + 1;
+        int min = int.MaxValue;
+        foreach (int i in wantedUnUsed)
+        {
+            int usedTime = Math.Max(lastTime, Math.Abs(startingValues[currentPrinter] - wantedValues[i]) + 1 + passedTime);
+            List<int> lleft = new List<int>(wantedUnUsed);
+            lleft.Remove(i);
+            if (low < 0)
+                min = Math.Min(min,Time(high, passedTime + Move(currentPrinter, high, printerDistance), usedTime, lowPrinter, high, lleft, printerDistance, startingValues, wantedValues));
+            else if (high >= wantedValues.Length)
+                min = Math.Min(min, Time(low, passedTime + Move(low, currentPrinter, printerDistance), usedTime, low, highPrinter, lleft, printerDistance, startingValues, wantedValues));
+            else
+            {
+                List<int> lright = new List<int>(lleft);
+                min = Math.Min(min, Math.Min(Time(low, passedTime + Move(low, currentPrinter, printerDistance), usedTime, low, highPrinter, lleft, printerDistance, startingValues, wantedValues), Time(high, passedTime + Move(currentPrinter, high, printerDistance), usedTime, lowPrinter, high, lright, printerDistance, startingValues, wantedValues)));
+            }
+        }
+        return min;
+    }
+    private int Move(int low, int high, int[] printerDistance)
+    {
+        int sum = 0;
+        for (int i = low; i < high; i++)
+        {
+            sum += printerDistance[i];
+        }
+        return sum;
+    }
+}
