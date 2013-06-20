1b1afc7ff3e4b3ac57cb98d7a7893b2c7aa42099
luchunminglu <luchunminglu@gmail.com>
2013-06-20 21:20:20 +0800

    c

--- TopCoder/TopCoder/Properties/AssemblyInfo.cs
+++ TopCoder/TopCoder/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
-// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
+// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
 // 则将该类型上的 ComVisible 属性设置为 true。
 [assembly: ComVisible(false)]
 
