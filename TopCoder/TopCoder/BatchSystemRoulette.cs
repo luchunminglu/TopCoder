5219eaea788638c8c4c361d2f04c923e66e4123e
chlu <luchunminglu@gmail.com>
2013-06-21 00:16:37 -0700

    c

--- TopCoder/TopCoder/BatchSystemRoulette.cs
+++ TopCoder/TopCoder/BatchSystemRoulette.cs
@@ -4,24 +4,52 @@ public class BatchSystemRoulette
 {
     public class Item
     {
-        public int duration;
-        public int ITh;
+        public long duration;
+        public long ITh;
+        public double Expection;
     }
 
     public class Items : IComparer<Items>
     {
-        public int DurationSum;
+        public long DurationSum;
         public List<Item> list = new List<Item>();
         public int Compare(Items x, Items y)
         {
-            return x.DurationSum - y.DurationSum;
+            return x.DurationSum - y.DurationSum>0?1:(x.DurationSum==y.DurationSum?0:-1);
         }
     }
+
+
+    public class ItemsList
+    {
+        public long DurationSum
+        {
+            get
+            {
+                if (duration > 0)
+                {
+                    return duration;
+                }
+                long sum1 = 0;
+                for (int i = 0; i < ListItems.Count; i++)
+                {
+                    sum1 += ListItems[i].DurationSum;
+                }
+                duration = sum1;
+                return sum1;
+            }
+        }
+        public List<Items> ListItems = new List<Items>();
+        public long SameValue;
+        private long duration = -1;
+
+    }
+
 
     public double[] expectedFinishTimes(int[] duration, string[] user)
     {
         Dictionary<string, Items> dics = new Dictionary<string, Items>();
-        for (int i = 0; i < duration.Length; i++)
+        for (long i = 0; i < duration.Length; i++)
         {
             Item item = new Item();
             item.ITh = i;
@@ -39,7 +67,7 @@ public class BatchSystemRoulette
         List<Items> items = new List<Items>();
         foreach (string key in dics.Keys)
         {
-            int sum = 0;
+            long sum = 0;
             foreach (Item item in dics[key].list)
             {
                 sum += item.duration;
@@ -49,39 +77,46 @@ public class BatchSystemRoulette
         }
         dics.Clear();
         items.Sort(new Items());
-        double[] result = new double[duration.Length];
-        double[] before = new double[items.Count+1];
-        int j = 0;
-        foreach (Items itemse in items)
+
+        Dictionary<long,ItemsList> dicItems = new Dictionary<long, ItemsList>();
+        foreach (Items item in items)
         {
-            int sum = 0;
-            foreach (Item item in itemse.list)
+            if (dicItems.ContainsKey(item.DurationSum))
             {
-                sum += item.duration;
+                dicItems[item.DurationSum].ListItems.Add(item);
             }
-            before[++j] = sum;
-        }
-        for (int i = 1; i < before.Length; i++)
-        {
-            before[i] += before[i - 1];
-        }
-        for (int i = 0; i < result.Length; i++)
-        {
-            int k = 0;
-            foreach (Items itemse in items)
+            else
             {
-                foreach (Item item in itemse.list)
-                {
-                    if (i == item.ITh)
-                    {
-                        result[i] = before[k] + item.duration/2.0 + itemse.DurationSum/2.0;
-                        break;
-                    }
-                }
-                k++;
+                ItemsList itemslist = new ItemsList();
+                itemslist.SameValue = item.DurationSum;
+                itemslist.ListItems.Add(item);
+                dicItems.Add(item.DurationSum, itemslist);
             }
         }
 
+        double sum1 = 0;
+        foreach (KeyValuePair<long, ItemsList> keyValuePair in dicItems)
+        {
+            double exp1 = keyValuePair.Value.SameValue*(keyValuePair.Value.ListItems.Count-1.0)/2.0;
+            foreach (Items items2 in keyValuePair.Value.ListItems)
+            {
+                double exp2 = items2.DurationSum/2.0;
+                foreach (Item _item in items2.list)
+                {
+                    _item.Expection = sum1 + exp1 + exp2 + _item.duration/2.0;
+                }
+            }
+            sum1 += keyValuePair.Value.DurationSum;
+        }
+        double[] result = new double[duration.Length];
+
+        foreach (Items itemse in items)
+        {
+            foreach (Item _item in itemse.list)
+            {
+                result[_item.ITh] = _item.Expection;
+            }
+        }
 
         return result;
     }
--- TopCoder/TopCoder/Program.cs
+++ TopCoder/TopCoder/Program.cs
@@ -8,8 +8,18 @@ namespace TopCoder
     {
         static void Main(string[] args)
         {
-            BatchSystemRoulette b = new BatchSystemRoulette();
-            var r = b.expectedFinishTimes(new int[] { 3, 2, 4, 1 }, new string[] { "mac taylor", "Mac Taylor", "Mac Taylor", "Peyton Driscoll" });
+            TicketPrinters printers = new TicketPrinters();
+            int result = printers.minTime(0,
+                                          new int[]
+                                              {
+                                                  851225, 629932, 193144, 395060, 75370, 749098, 159313, 673616, 586646,
+                                                  153568, 495022, 279116, 899591, 734646, 587157
+                                              },
+                                          new int[]
+                                              {
+                                                  283373, 305651, 162774, 850954, 627964, 494793, 595841, 386664, 883734,
+                                                  460062, 447811, 637987, 628622, 260559, 980019, 226273
+                                              }, new int[] { 780911, 957634, 866639, 567511, 928070, 94303, 42794, 312249, 49545, 532165, 501539, 774903, 787307, 869676, 154728, 460386 });
         }
     }
 }
