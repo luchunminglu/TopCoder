5e93187d78636f4bdf1eb3571ca77714bc60b0d2
luchunminglu <luchunminglu@gmail.com>
2014-05-11 21:43:28 -0700

    cc

---
+++ TopCoder/TopCoder/Passed/Time.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Time
+{
+    public String whatTime(int seconds)
+    {
+        TimeSpan timeSpan = new TimeSpan(0,0,0,seconds);
+        return timeSpan.Hours + ":" + timeSpan.Minutes + ":" + timeSpan.Seconds;
+    }
+}
+
--- TopCoder/TopCoder/Program.cs
+++ TopCoder/TopCoder/Program.cs
@@ -8,23 +8,7 @@ namespace TopCoder
     {
         static void Main(string[] args)
         {
-            Egalitarianism eg = new Egalitarianism();
-            string[] arr = new string[]{"NYYNNNNYYYYNNNN",
- "YNNNYNNNNNNYYNN",
- "YNNYNYNNNNYNNNN",
- "NNYNNYNNNNNNNNN",
- "NYNNNNYNNYNNNNN",
- "NNYYNNYNNYNNNYN",
- "NNNNYYNNYNNNNNN",
- "YNNNNNNNNNYNNNN",
- "YNNNNNYNNNNNYNN",
- "YNNNYYNNNNNNNNY",
- "YNYNNNNYNNNNNNN",
- "NYNNNNNNNNNNNNY",
- "NYNNNNNNYNNNNYN",
- "NNNNNYNNNNNNYNN",
- "NNNNNNNNNYNYNNN"};
-            Console.WriteLine(eg.maxDifference(arr,747));
+           
         }
     }
 
