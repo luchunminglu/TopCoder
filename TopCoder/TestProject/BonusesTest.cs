af258eb5f27eada289df4b69e3f158f3ce8b3f44
luchunminglu <luchunminglu@gmail.com>
2014-05-12 00:24:33 -0700

    cc

---
+++ TopCoder/TestProject/BonusesTest.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    
+    
+    /// <summary>
+    ///This is a test class for BonusesTest and is intended
+    ///to contain all BonusesTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class BonusesTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for getDivision
+        ///</summary>
+        [TestMethod()]
+        public void getDivisionTest()
+        {
+            Bonuses target = new Bonuses(); // TODO: Initialize to an appropriate value
+            int[] points = { 485, 324, 263, 143, 470, 292, 304, 188, 100, 254, 296,
+ 255, 360, 231, 311, 275,  93, 463, 115, 366, 197, 470 }; // TODO: Initialize to an appropriate value
+            int[] expected = { 8,  6,  4,  2,  8,  5,  5,  3,  1,  4,  5,  4,  6,  3,  5,  4,  1,  8,
+  1,  6,  3,  8 }; // TODO: Initialize to an appropriate value
+            int[] actual;
+            actual = target.getDivision(points);
+            bool result = expected.SequenceEqual(actual);
+            Console.WriteLine(result);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
---
+++ TopCoder/TopCoder/Passed/Bonuses.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Bonuses
+{
+    public int[] getDivision(int[] points)
+    {
+        int sum = points.Sum();
+        //index, points, bonus
+        var tuples = new List<Tuple>();
+        for (int i = 0; i < points.Length; i++)
+        {
+            tuples.Add(new Tuple(i, points[i], 0));
+        }
+
+        tuples = tuples.OrderByDescending(r => r.Item2).ThenBy(r => r.Item1).ToList();
+        int extras = 0;
+        foreach (Tuple tuple in tuples)
+        {
+            tuple.Item3 = tuple.Item2*100/sum;
+            extras += tuple.Item2*100%sum;
+        }
+        extras = extras/sum;
+        for (int i = 0; i < extras; i++)
+        {
+            tuples[i].Item3++;
+        }
+        return tuples.OrderBy(r => r.Item1).Select(r => r.Item3).ToArray();
+    }
+
+    private class Tuple
+    {
+        public readonly int Item1;
+        public readonly int Item2;
+        public int Item3;
+
+        public Tuple(int i1, int i2, int i3)
+        {
+            Item1 = i1;
+            Item2 = i2;
+            Item3 = i3;
+        }
+    }
+}
