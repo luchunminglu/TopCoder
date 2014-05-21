08a66e9f0af76199fdab4d8e24ef4f2a0ffef2d2
luchunminglu <luchunminglu@gmail.com>
2014-05-20 19:32:54 -0700

    cc

---
+++ TopCoder/TestProject/MasterbrainTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    
+    
+    /// <summary>
+    ///This is a test class for MasterbrainTest and is intended
+    ///to contain all MasterbrainTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class MasterbrainTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for possibleSecrets
+        ///</summary>
+        [TestMethod()]
+        public void possibleSecretsTest()
+        {
+            Masterbrain target = new Masterbrain(); // TODO: Initialize to an appropriate value
+            string[] guesses = { "6172", "6162", "3617" }; // TODO: Initialize to an appropriate value
+            string[] results = { "3b 0w", "2b 1w", "0b 3w" }; // TODO: Initialize to an appropriate value
+            int expected = 14; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.possibleSecrets(guesses, results);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
---
+++ TopCoder/TopCoder/Masterbrain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Masterbrain
+{
+    public int possibleSecrets(String[] guesses, String[] results)
+    {
+        var list = new List<string>();
+        for (int i = 1; i <= 7; i++)
+        {
+            for (int j = 1; j <= 7; j++)
+            {
+                for (int k = 1; k <= 7; k++)
+                {
+                    for (int l = 1; l <= 7; l++)
+                    {
+                        list.Add("" + i + j + k + l);
+                    }
+                }
+            }
+        }
+        List<string> totalList = new List<string>();
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            List<string> all = list.ToList();
+            all.RemoveAll(s => Result(s, guesses[i]) == results[i]);
+            for (int j = 0; j < results.Length; j++)
+            {
+                if (j == i)
+                {
+                    //assume i is the wrong one
+                    continue;
+                }
+                all.RemoveAll(s => Result(s, guesses[j]) != results[j]);
+            }
+            totalList.AddRange(all);
+            totalList.Distinct();
+        }
+
+        return totalList.Count;
+    }
+
+    public string Result(string secret, string guess)
+    {
+        int black = 0;
+        int white = 0;
+        bool[] array = new bool[secret.Length];
+
+        for (int i = 0; i < guess.Length; i++)
+        {
+            for (int j = 0; j < secret.Length; j++)
+            {
+                if (!array[j] && secret[j] == guess[i])
+                {
+                    if (i == j)
+                    {
+                        black++;
+                        array[j] = true;
+                        break;
+                    }
+                    else
+                    {
+                        white++;
+                        array[j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return black + "b " + white + "w";
+    }
+}
